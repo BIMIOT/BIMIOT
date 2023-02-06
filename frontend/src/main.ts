7ac960fd03805d179e01cc92143596c4a84eaf9d
Leane <leane@LeanedeMacBook-Air.local>
2023-02-06 16:52:31 +0100

    feat: call create project api

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import router from './router'
 
 loadFonts()
 
 createApp(App)
-  .use(vuetify)
+  .use(vuetify).use(router)
   .mount('#app')
