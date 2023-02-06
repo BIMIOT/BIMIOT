2d66ac8ad72a1f385e77643bf53d3488c22201fa
Tietienne <tietienne77@gmail.com>
2023-02-06 14:35:33 +0100

    fix: front config for install

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+    outputDir: 'target/dist',
+      assetsDir: 'static',
 
   pluginOptions: {
     vuetify: {
