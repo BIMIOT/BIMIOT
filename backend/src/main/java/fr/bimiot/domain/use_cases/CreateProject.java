a7e32235e9271d74d4492031568860c36c197f79
Yan LI <yan.li@edu.univ-eiffel.fr>
2023-02-08 10:21:19 +0100

    feat: save csv file

--- backend/src/main/java/fr/bimiot/domain/use_cases/CreateProject.java
+++ backend/src/main/java/fr/bimiot/domain/use_cases/CreateProject.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 public class CreateProject {
 
     private final static String IFC_FILE = "ifcFile.ifc";
+    private final static String DATASET_FILE = "dataset.csv";
 
     public ProjectDirectory execute(ProjectDirectory projectDirectory){
         try{
@@ -19,6 +20,9 @@ public class CreateProject {
             Files.createDirectory(Paths.get("Projects/" + projectDirectory.name()));
             Files.createFile(Paths.get("Projects/" + projectDirectory.name() + "/" + IFC_FILE));
             Files.write(Paths.get("Projects/"+projectDirectory.name()+"/" + IFC_FILE), projectDirectory.ifcFile().getBytes());
+
+            Files.createFile(Paths.get("Projects/" + projectDirectory.name() + "/" + DATASET_FILE));
+            Files.write(Paths.get("Projects/"+projectDirectory.name()+"/" + DATASET_FILE), projectDirectory.datasetFile().getBytes());
         }catch (Exception e){
             throw new AssertionError(e);
         }
