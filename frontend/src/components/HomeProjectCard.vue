2765ef8f0f93ca77ba25c29166918a1c497e34ea
Tietienne <tietienne77@gmail.com>
2023-02-21 10:38:42 +0100

    feat: better buttons on main page

--- frontend/src/components/HomeProjectCard.vue
+++ frontend/src/components/HomeProjectCard.vue
@@ -5,26 +5,26 @@
     </v-card-title>
     <v-card-actions class="justify-center">
       <v-btn
-        variant="outlined"
-        color="green"
-        @click="sendProjectName"
+          variant="elevated"
+          color="primary"
+          @click="sendProjectName"
       >
         Simulation
       </v-btn>
       <v-btn
-            variant="outlined"
-            color="blue"
-            @click="this.dialog = true"
-        >
-          Modifier
-        </v-btn>
-        <v-btn
-            variant="outlined"
-            color="red"
-            @click="this.dialog = true"
-        >
-          Supprimer
-        </v-btn>
+          variant="outlined"
+          icon="mdi-pencil"
+          color="secondary"
+          size="x-small"
+          @click="this.dialog = true"
+      ></v-btn>
+      <v-btn
+          variant="outlined"
+          icon="mdi-delete"
+          color="error"
+          size="x-small"
+          @click="this.dialog = true"
+      ></v-btn>
     </v-card-actions>
   </v-card>
 
