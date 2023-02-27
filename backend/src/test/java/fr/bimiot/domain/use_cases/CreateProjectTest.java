3d691427aa675b2fdfaaaecb522d96c0c63a8e19
bricemikembo <MIKbri2000>
2023-02-27 17:05:22 +0100

    setting sonar qube

--- backend/src/test/java/fr/bimiot/domain/use_cases/CreateProjectTest.java
+++ backend/src/test/java/fr/bimiot/domain/use_cases/CreateProjectTest.java
@@ -1,10 +1,20 @@
 package fr.bimiot.domain.use_cases;
 
+import fr.bimiot.domain.entities.Project;
+import fr.bimiot.domain.exception.DomainException;
 import fr.bimiot.domain.use_cases.providers.ProjectDatabaseProvider;
+import fr.bimiot.fixtures.ProjectFixture;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class CreateProjectTest {
@@ -15,7 +25,7 @@ class CreateProjectTest {
     ProjectDatabaseProvider projectDatabaseProvider;
 
     private static final String PROJECT_ID = "project_id";
-/*
+
     @Test
     void execute_shouldReturnProjectId() throws IOException, DomainException {
         Project project = ProjectFixture.aProjectWithoutSensorsAndWithoutId();
@@ -23,5 +33,5 @@ class CreateProjectTest {
         String result = createProject.execute(project);
         assertNotNull(result);
         assertEquals(PROJECT_ID, result);
-    }*/
+    }
 }
